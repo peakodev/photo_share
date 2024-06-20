b0a2dc941d4e714fac352b628b0654c1c8a9f0f9
Nazar Salo <peakodev@gmail.com>
2024-06-20 13:10:52 +0300

    test

--- app/database/db.py
+++ app/database/db.py
@@ -10,7 +10,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-# Dependency
+# D ependency
 def get_db():
     db = SessionLocal()
     try:
