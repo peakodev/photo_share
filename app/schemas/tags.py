dfd68b8e12864a7d35a5efee7928e7628c95b8c8
Nazar Salo <peakodev@gmail.com>
2024-06-24 00:41:01 +0300

    unittest: users. auth, email service

--- app/schemas/tags.py
+++ app/schemas/tags.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
 
 
-
 class TagModel(BaseModel):
     text: str
+
 
 class TagDB(TagModel):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
